729738bc89d68ef21ab6502e1795773a44be8df6
whiterasbk <whiterasbk@qq.com>
2022-10-23 02:31:06 +0800

    refactor rename

--- service/src/test/kotlin/org/mider/produce/ApplicationTest.kt
+++ service/src/test/kotlin/org/mider/produce/ApplicationTest.kt
@@ -1,13 +1,4 @@
 package org.mider.produce
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import kotlin.test.*
-import io.ktor.server.testing.*
-import org.mider.produce.plugins.*
-
 class ApplicationTest {
 }
