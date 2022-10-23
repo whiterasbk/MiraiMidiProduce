d5867645507985f81be1e79da70579ea41812599
whiterasbk <whiterasbk@qq.com>
2022-10-23 17:42:14 +0800

    add: index

--- service/src/main/kotlin/org/mider/produce/service/plugins/Routing.kt
+++ service/src/main/kotlin/org/mider/produce/service/plugins/Routing.kt
@@ -1,8 +1,10 @@
 package org.mider.produce.service.plugins
 
 import com.fasterxml.jackson.databind.SerializationFeature
+import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
+import io.ktor.server.http.content.*
 import io.ktor.server.plugins.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
@@ -11,13 +13,12 @@ import io.ktor.server.routing.*
 import io.ktor.util.logging.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.mider.produce.core.Configuration
 import org.mider.produce.core.generate
+import org.mider.produce.core.utils.toPinyin
 import org.mider.produce.service.data.ServiceParameter
-import org.mider.produce.service.logger.AppLogger
 import org.mider.produce.service.utlis.getConfiguration
 import org.mider.produce.service.utlis.hash
-import org.mider.produce.service.utlis.setupViaEnvironment
+import whiter.music.mider.xml.LyricInception
 import java.io.File
 
 fun Application.configureRouting() {
@@ -28,15 +29,19 @@ fun Application.configureRouting() {
         }
     }
 
+    LyricInception.replace = { it.toPinyin() }
+
     val logger = log
     val (config, workspace) = getConfiguration(this)
 
-    log.info(">>>>>" + config.debug)
+    workspace.listFiles { dir, _ ->
+        dir.delete()
+    }
 
     // Starting point for a Ktor app:
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondRedirect("static/index.html")
         }
 
         post("/api") {
@@ -75,7 +80,7 @@ fun Application.configureRouting() {
                         file.writeBytes(withContext(Dispatchers.IO) {
                             stream.readAllBytes()
                         })
-                        logger.info("generate file: $file at: ${file.absolutePath}")
+                        // logger.info("generate file at: ${file.absolutePath}")
                     }
 
                     links += mapOf(name to "/generated/${file.name}")
@@ -92,6 +97,56 @@ fun Application.configureRouting() {
                     "links" to links
                 ))
 
+            } catch (e: Throwable) {
+                call.respond(mapOf(
+                    "status" to "failure",
+                    "message" to e.message
+                ))
+                logger.error(e)
+            }
+        }
+
+        post("/direct-api") {
+            val parameter = try {
+                call.receive<ServiceParameter>()
+            } catch (e: BadRequestException) {
+                call.respond(mapOf(
+                    "status" to "failure",
+                    "message" to "midercode is required: $e"
+                ))
+                logger.error(e)
+                return@post
+            }
+
+            parameter.copy(config)
+
+            try {
+
+                val (result, streamList) = config.generate(parameter.midercode)
+                val (stream, name) = streamList[0]
+                val contentType: ContentType = when {
+                    name.contains(".") -> name.split(".").last().let {
+                        when (it) {
+                            "png" -> ContentType.Image.PNG
+                            "mp3" -> ContentType.Audio.MPEG
+                            "mid" -> ContentType.Audio.Any
+                            "silk" -> ContentType.Audio.Any
+                            else -> ContentType.Any
+                        }
+                    }
+
+                    else -> when {
+                        result.isUploadMidi -> ContentType.Audio.Any
+                        result.isRenderingNotation -> ContentType.Any
+                        else -> ContentType.Audio.MPEG
+                    }
+                }
+
+                call.respondBytes(contentType = contentType) {
+                    withContext(Dispatchers.IO) {
+                        stream.readAllBytes()
+                    }
+                }
             } catch (e: Throwable) {
                 call.respond(mapOf(
                     "status" to "failure",
@@ -120,6 +175,10 @@ fun Application.configureRouting() {
                 ))
                 logger.error(e)
             }
+        }
+
+        static("/static") {
+            resources("static")
         }
     }
 }
